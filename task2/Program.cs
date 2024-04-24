31ccd250d822438aec38173d975605783dd9b005
Ramil <ramil.mirzoev@gmail.comm>
2024-04-24 12:16:52 +0400

    написали для себя код шаблон

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,19 +1,44 @@
-﻿int[] arr = new int[5];
-arr[0] = 5;
-arr[1] = 7;
-arr[2] = 5;
-arr[3] = 4;
-arr[4] = 3;
-Console.WriteLine(arr[0]);
-Console.WriteLine(arr[1]);
-Console.WriteLine(arr[2]);
-Console.WriteLine(arr[3]);
-Console.WriteLine(arr[4]);
-// Console.WriteLine(arr[5]);
+﻿// int[] arr = new int[5];
+// arr[0] = 5;
+// arr[1] = 7;
+// arr[2] = 5;
+// arr[3] = 4;
+// arr[4] = 3;
+// Console.WriteLine(arr[0]);
+// Console.WriteLine(arr[1]);
+// Console.WriteLine(arr[2]);
+// Console.WriteLine(arr[3]);
+// Console.WriteLine(arr[4]);
+// // Console.WriteLine(arr[5]);
 
-int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+// int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
 
 
-int[] arr3 = { 1, 2, 3, 4, 5 };
+// int[] arr3 = { 1, 2, 3, 4, 5 };
 
 // arr = {1,2,3,4,5}
+
+Console.WriteLine("ВВедите размер массива");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+int[] numbers = new int [n];
+while (i < n)
+{
+    Console.WriteLine($"ВВедите {i} число массива");
+    numbers [i] = Convert.ToInt32(Console.ReadLine());
+    
+    i = i + 1;
+}
+
+int i1 =0;
+
+while (i1 < n)
+{
+    Console.Write($"{numbers [i1] } " );
+    i1 = i1 + 1;
+
+}
+
+
--- task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423bf39f84200bea372b85eefb93b78c4181c78d")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
