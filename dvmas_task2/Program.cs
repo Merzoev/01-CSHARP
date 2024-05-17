c1f8a8b073cc649b111395d0279866bbb7c63648
Ramil <ramil.mirzoev@gmail.comm>
2024-05-17 12:51:03 +0400

    задача Дмассив

---
+++ dvmas_task2/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и
+// т.д.
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)  // создание двухмерного массива
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rand.Next(min, max); 
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)        // вывод двухмерного массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int SumMainDiagonal(int[,] matrix)      // суммирование главной диагонали
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += matrix[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+
+
+Console.WriteLine("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное число массива");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное число массива");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = CreateMatrix(rows, columns, min, max);
+PrintMatrix(matrix);
+Console.WriteLine(SumMainDiagonal(matrix));
