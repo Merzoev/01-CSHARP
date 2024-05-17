5a5863dfa929f38997b09db204a1b35c7316dfa5
Ramil <ramil.mirzoev@gmail.comm>
2024-05-17 12:50:14 +0400

    задачи по двухмерным массивам

---
+++ dMmassivi/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿int[,] matrix = new int[3, 4];
+
+ Random rnd = new Random(); // Random number generator
+ 
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.Next (1,11);
+    }
+}
+
+// Print the matrix
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+int [,] CreatMatrix []
